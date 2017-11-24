53faf9904673722abc202d418cc2d5842398742c
Rohit Vashisht <rohit43@users.noreply.github.com>
2017-11-23 19:40:31 -0800

    added ageGender computation

---
+++ R/getAgeGender.R
@@ -0,0 +1,32 @@
+# @file functions
+#
+# Copyright 2017 Observational Health Data Sciences and Informatics
+#
+# This file is part of:
+#  ----------------------------------------------
+#  DiabetesTxPath
+#  ----------------------------------------------
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @author Stanford University Center for Biomedical Informatics - Shah Lab
+# @author Rohit Vashisht
+#
+#' @title
+#' getAgeGender
+#'
+#' @author
+#' Rohit Vashisht
+#'
+#' @details
+#' This function can be used to compute the age and gender of patients for each outcome and for each treatment and
+#' comparator cohort.
